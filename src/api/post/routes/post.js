ea2734ca93a3bba220b207f235bf1ce263e2305d
adjeiessel <essel.adjei-mensah@zeppelin.com>
2023-02-19 18:17:29 +0100

    fixes bug

--- src/api/post/routes/post.js
+++ src/api/post/routes/post.js
@@ -6,4 +6,19 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::post.post');
+module.exports = createCoreRouter('api::post.post'/** This is a way to customise a core route */,{
+  prefix:'', //tags => /test/tags
+  only:[] ,// allows you to specify which methods you want to include
+  except:['create'],
+  config:{
+    find:{
+      auth:false,// setting it to false, disables the strapi JWT auth system for this route
+      policies:[], //can be added to the route to which are added to the default policies 
+      middleware:[]// middleware can be added to the routes to perform special functions
+    },
+    findOne:{},
+    create:{},
+    update:{},
+    delete:{}
+  }
+});
